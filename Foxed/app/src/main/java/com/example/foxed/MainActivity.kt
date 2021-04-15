8d0677c366aeebe073e63af65b2058b746976a10
xfillmore <xuedan.fillmore@colorado.edu>
2021-04-15 10:28:48 -0600

    lab8 | more stuff

--- Foxed/app/src/main/java/com/example/foxed/MainActivity.kt
+++ Foxed/app/src/main/java/com/example/foxed/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.foxed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun loadNewImage(view: View) {
+        val imgid = Random.nextInt(1, 122)
+        //
+    }
+
 }
--- Foxed/app/src/main/java/com/example/foxed/util/DataJSON.kt
+++ Foxed/app/src/main/java/com/example/foxed/util/DataJSON.kt
@@ -1,12 +1,46 @@
 package com.example.foxed.util
 
 import android.content.Context
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.HttpResponse
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.foxed.model.FoxViewModel
+import org.json.JSONException
+import org.json.JSONObject
 
 class DataJSON {
     val furl = "https://randomfox.ca/?i="
 
-    fun loadJSON (context: Context, foxViewModel: FoxViewModel) {
-        
+    fun loadJSON (context: Context, foxViewModel: FoxViewModel, imgid: Int) {
+        val url = furl + imgid.toString()
+
+        val queue = Volley.newRequestQueue(context)
+        val stringRequest = StringRequest(
+                Request.Method.GET,
+                url,
+                { response ->
+                    parseJSON(response, FoxViewModel)
+                },
+                {
+                    Log.e("Error", error("Request failed."))
+                }
+        )
+
+        queue.add(stringRequest)
+    }
+
+    fun parseJSON(response: String, viewModel: FoxViewModel) {
+        val imgurl = "https://randomfox.ca/images/"
+        try {
+            val jsonObject = JSONObject(response)
+            val result = jsonObject.getJSONArray("results")
+            Log.i("JSON response", result as String)
+        }
+        catch (err: JSONException) {
+            err.printStackTrace()
+        }
+
     }
 }
