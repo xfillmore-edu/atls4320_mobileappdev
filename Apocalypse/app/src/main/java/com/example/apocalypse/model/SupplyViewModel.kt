17d95057e4cae22c5b0d7c7bc55e84b0b6eeb799
xfillmore <xuedan.fillmore@colorado.edu>
2021-04-27 04:42:31 -0600

    lab9 | setup for shared prefs

--- Apocalypse/app/src/main/java/com/example/apocalypse/model/SupplyViewModel.kt
+++ Apocalypse/app/src/main/java/com/example/apocalypse/model/SupplyViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.apocalypse.model
 
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.apocalypse.util.SharedPreferencesData
 
-class SupplyViewModel : ViewModel() {
+class SupplyViewModel (application: Application) : AndroidViewModel(application) {
     val supplyList = MutableLiveData<ArrayList<Item>>()
     private var newList = ArrayList<Item>()
 
@@ -15,5 +20,21 @@ class SupplyViewModel : ViewModel() {
     fun useSupply(sp: Item) {
         newList.remove(sp)
         supplyList.value = newList
+    }
+
+    // shared preferences content
+    @SuppressLint("StaticFieldLeak")
+    val context: Context = application.applicationContext
+    private val spData = SharedPreferencesData()
+
+    fun loadData() {
+        newList = spData.loadData(context)
+        supplyList.value = newList
+    }
+
+    fun saveData() {
+        supplyList.value?.let {
+            spData.saveData(supplyList.value!!, context)
+        }
     }
 }
--- Apocalypse/app/src/main/java/com/example/apocalypse/util/SharedPreferencesData.kt
+++ Apocalypse/app/src/main/java/com/example/apocalypse/util/SharedPreferencesData.kt
@@ -1,4 +1,43 @@
 package com.example.apocalypse.util
 
+import android.content.Context
+import com.example.apocalypse.model.Item
+
 class SharedPreferencesData {
+    private val preferenceFile = "SUPPLIES"
+
+    fun saveData (supplyList: ArrayList<Item>, context: Context) {
+        // access shared pref file
+        val sp = context.getSharedPreferences(preferenceFile, Context.MODE_PRIVATE)
+
+        // editor for writing to shared pref file
+        val ed = sp.edit()
+        if (supplyList != null) {
+            ed.putInt("numItems", supplyList.size)
+        }
+
+        // put items into strings of data
+        for ((ind, item) in supplyList.withIndex()) {
+            ed.putString("sn$ind", item.name)
+        }
+
+        // save data
+        ed.apply()
+    }
+
+    fun loadData (context: Context) : ArrayList<Item> {
+        var returningList = ArrayList<Item>()
+
+        // access shared prefs file
+        val sp = context.getSharedPreferences(preferenceFile, Context.MODE_PRIVATE)
+        val size = sp.getInt("numItems", 0)
+
+        // pull items
+        for (i in 0 until size) {
+            returningList.add(Item(sp.getString("sn$i", "")!!))
+        }
+
+        return returningList
+    }
+
 }
